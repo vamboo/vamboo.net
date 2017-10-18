a64052e17c61653094502215f2600e0f71ed5859
Sosuke Suzuki <aosukeke@gmail.com>
2017-10-18 17:50:27 +0900

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'budle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: __dirname + '/src',
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.pug$/,
+        exclude: /node_modules/,
+        use: 'pug-loader'
+      },
+      {
+        test: /\.styl$/,
+        include: __dirname + '/src',
+        exclude: /node_modules/,
+        use: [{
+          loader: 'style-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'css-loader',
+          options: {
+            localIdentName: '[sha512:hash:base32]-[name]-[local]',
+            modules: true,
+            sourceMap: true 
+          }
+        }, {
+          loader: 'stylus-loader',
+          options: { sourceMap: true }
+        }]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    })
+  ]
+}
